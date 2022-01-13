e134ab21f8837d67850a56b2e129a873a3a1054c
TimA314 <ci0d7j9m@mailer.me>
2022-01-13 15:32:32 -0800

    set up layout and routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ import {
 } from "@ant-design/icons/lib/icons";
 import icon from "../images/cryptocurrency.png";
 
-export const Navbar = () => {
+export default function Navbar() {
   return (
     <div className="nav-container">
       <div className="logo-container">
@@ -20,6 +20,20 @@ export const Navbar = () => {
         </Typography.Title>
         {/* <Button className="menu-control-container"></Button> */}
       </div>
+      <Menu theme="dark">
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to="/cryptocurrencies">Cryptourrencies</Link>
+        </Menu.Item>
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link to="/exchanges">Exchanges</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to="/news">News</Link>
+        </Menu.Item>
+      </Menu>
     </div>
   );
-};
+}
