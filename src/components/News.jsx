0b5f7ce0d0fd8f19dfc74f80c50299b9aadced45
TimA314 <ci0d7j9m@mailer.me>
2022-01-14 10:36:43 -0800

    add crypto details variables

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Select, Typography, Row, Col, Avatar, Card } from "antd";
 import moment from "moment";
 
 import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const { Text, Title } = Typography;
 const { Option } = Select;
@@ -10,8 +11,10 @@ const { Option } = Select;
 const demoImage =
   "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcimg.co%2Fw%2Frw-logo%2F2%2F5cf%2Fec8853832b.png&f=1&nofb=1";
 export default function News({ simplified }) {
+  const [newsCategory, setNewsCategory] = useState("cryptocurrency");
+  const { data } = useGetCryptosQuery(100);
   const { data: cryptoNews } = useGetCryptoNewsQuery({
-    newsCategory: "Cryptocurrency",
+    newsCategory,
     count: simplified ? 6 : 18,
   });
 
@@ -21,6 +24,25 @@ export default function News({ simplified }) {
 
   return (
     <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a Crypto"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) > 0
+            }
+          >
+            <Option value="cryptocurrency">Cryptocurrency</Option>
+            {data?.data?.coins.map((coin) => (
+              <Option value={coin.name}>{coin.name}</Option>
+            ))}
+          </Select>
+        </Col>
+      )}
       {cryptoNews.value.map((news, i) => (
         <Col xs={24} sm={12} lg={8} key={i}>
           <Card hoverable className="news-card">
